7f35e1b14541fa98bf59fe6ec2978b29a038e35a
CymerR <sergemers109@gmail.com>
2021-08-02 11:17:55 +0300

    Parser work in progress

--- src/ru/cmr/interpret/Interpreter.java
+++ src/ru/cmr/interpret/Interpreter.java
@@ -1,6 +1,9 @@
 package ru.cmr.interpret;
 
 import ru.cmr.expr.Expression;
+import ru.cmr.expr.impl.AddExpression;
+import ru.cmr.expr.impl.NumExpression;
+import ru.cmr.expr.impl.SubtractExpression;
 
 import java.util.ArrayDeque;
 
@@ -13,13 +16,21 @@ public class Interpreter {
         opsStack = new ArrayDeque<>();
 
         var strs = input.split(" ");
-        var parser = new Parser();
         for (String word : strs) {
-
+            if (word.equals("+")) {
+                opsStack.addLast(AddExpression.start());
+            } else if (word.equals("-")) {
+                opsStack.addLast(SubtractExpression.start());
+            } else {
+                numStack.addLast(new NumExpression(Double.parseDouble(word)));
+            }
         }
     }
 
     public double eval() {
-        return 0;
+        for (var op : opsStack) {
+            
+        }
+        return opsStack.getFirst().eval();
     }
 }
--- src/ru/cmr/interpret/Parser.java
+++ src/ru/cmr/interpret/Parser.java
@@ -1,5 +1,17 @@
 package ru.cmr.interpret;
 
+import ru.cmr.expr.Expression;
+import ru.cmr.expr.impl.NumExpression;
+
 public class Parser {
 
+    private final String word;
+
+    public Parser(String word) {
+        this.word = word;
+    }
+
+    public Expression parse() {
+        return new NumExpression(0);
+    }
 }
