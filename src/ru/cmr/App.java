494b495e0dcf5c3db9d0044f63b4f6a0f3d67b91
CymerR <sergemers109@gmail.com>
2021-08-09 00:02:50 +0300

    REPL like calculator

--- src/ru/cmr/App.java
+++ src/ru/cmr/App.java
@@ -1,8 +1,25 @@
 package ru.cmr;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class App {
     public static void main(String[] args) {
-        var s = "-1 + 2 / 2";
-        System.out.println(new Calculator(s).calc());
+        var sc = new Scanner(System.in);
+        while(true) {
+            var s = sc.nextLine().trim();//"-1 + 2 / 2";
+            if (s.equalsIgnoreCase("q")) break;
+//            if (!Pattern.matches("\\d+|\\s+|[+\\-*/]+", s)) {
+//                System.err.printf(
+//                        "Error! \nUsage:\t %s \nInstead was: { '%s' } \n",
+//                        "Type correct input!",
+//                        s
+//                );
+//                continue;
+//            }
+            System.out.println(new Calculator(s).calc());
+        }
+        System.out.println("BB!");
     }
 }
